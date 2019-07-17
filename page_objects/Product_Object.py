1ea98975bc9bba6557b5a15014d5b6a2029ded77
Shrihari R <shrihari.r@qxf2.com>
2019-07-17 15:53:40 +0530

    By adding return statement in 'get current cart quantity' fixed error 4

--- page_objects/Product_Object.py
+++ page_objects/Product_Object.py
@@ -120,6 +120,8 @@ class Product_Object():
         positive="The cart currently has %d items"%self.CART_QUANTITY,
         negative="")
 
+        return self.CART_QUANTITY
+
     def add_product(self,product_name):
         "Add the lowest priced product with the filter condition in name"
         before_cart_quantity = self.get_current_cart_quantity() 
