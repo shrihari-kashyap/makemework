07174cd8ebec1b8c532bc9d6f8c5c1a662642097
Shrihari R <shrihari.r@qxf2.com>
2019-07-23 10:38:56 +0530

    Added values to frame

--- conf/e2e_weather_shopper_conf.py
+++ conf/e2e_weather_shopper_conf.py
@@ -2,3 +2,10 @@
 This conf contains the data needed by test_e2e_purchase_product.py
 """
 PURCHASE_LOGIC = {"moisturizers":['aloe'],"sunscreens":['spf-50']}
+
+email_field = "shrihari@gmail.com"
+card_number = "3566002020360505"
+expiry_date="10/20"
+cvc_number="356"
+zip_code="560019"
+mobile_number = "8123038116"
